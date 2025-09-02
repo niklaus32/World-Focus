45f14f6701c2c90008ed5842fd2d15656777b812
Kot <89110272+Kot6603@users.noreply.github.com>
2025-09-02 22:41:18 +1200

    added cursor-pointer to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ function Button({ children, onClick, type = "button" }) {
     <button
       type={type}
       onClick={onClick}
-      className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg shadow-md transition"
+      className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg shadow-md transition cursor-pointer"
     >
       {children}
     </button>
